a7bfb3bba1babc895697ebaab82d182a844c3144
liepinaievaa-maker <liepinaievaa@gmail.com>
2025-12-20 12:58:23 +0200

    Add functions for JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+/**
+ * Functions
+ */
+function startGame() {
+
+}
+
+function restartGame() {
+
+}
+
+/**
+ * Event Listeners
+ */
+document.getElementById('start-button').addEventListener('click', startGame);
+document.getElementById('restart-button').addEventListener('click', restartGame);
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer () {
+
+}
+
+function incrementScore() { 
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function incrementTie() {
+
+}
+/**
+ * Display photos and results
+ */
+function displayChoice() {
+
+}
+
+function displayRoundResult() { 
+
+}
+
+function updateRoundInfo () {
+ 
+}
+
+function enableChoiceButtons() {
+
+}
+/**
+ * End Game
+ */
+function endGame() { 
+}
+
