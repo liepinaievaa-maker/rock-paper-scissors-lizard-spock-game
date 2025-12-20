0fd4f97584f5ad37c549d396888af1975a0f22c7
liepinaievaa-maker <liepinaievaa@gmail.com>
2025-12-20 13:41:37 +0200

    Add functions for Start and Restart Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,38 @@ const beats = {
     Lizard : ['Spock', 'Paper'],
     Spock : ['Scissors', 'Rock']
 };
+/**
+ * Game Variables
+ */
+let round = 1;
+let maxRounds = 5;
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+let gameActive = false;
 
 /**
  * Functions
  */
 function startGame() {
-
+    gameActive = true;
+    resultMessage.textContent = 'Game Started! Make your choice.';
+    enableChoiceButtons(true);
 }
 
 function restartGame() {
-
+    round = 1;
+    playerScore = 0;
+    computerScore = 0;
+    tieScore = 0;
+    gameActive = false;
+    playerChoiceImage.src = '';
+    computerChoiceImage.src = '';
+    roundInfo.textContent = '';
+    scoreInfo.textContent = '';
+    resultMessage.textContent = 'Game Restarted! Click Start to play again.';
+    updateRoundInfo();
+    updateScoreBoard();
 }
 
 /**
